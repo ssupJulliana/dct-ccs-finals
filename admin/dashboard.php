f51d535eb3a797eead63be359cab9b91ce317242
ssupJulliana <julls.castaneda02@gmail.com>
2024-11-21 00:15:33 +0800

    Implement the login feature

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,15 @@
 <!-- Template Files here -->
+<?php
+
+
+?>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Dashboard</title>
+</head>
+<body class="bg-secondary-subtle">
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
     <h1 class="h2">Dashboard</h1>        
     
@@ -37,4 +48,6 @@
         </div>
     </div>    
 </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 <!-- Template Files here -->
